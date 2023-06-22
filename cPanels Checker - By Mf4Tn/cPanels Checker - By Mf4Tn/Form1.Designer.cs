0c724fb020422e3f84ee50da4ffe96004ef0860d
Mf4Tn <75338199+Mf4Tn@users.noreply.github.com>
2023-06-22 21:25:41 +0100

    Add files via upload

---
+++ cPanels Checker - By Mf4Tn/cPanels Checker - By Mf4Tn/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace cPanels_Checker___By_Mf4Tn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load cPanels File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(206, 151);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "HOST|USER|PASS";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Checking";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(206, 160);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "HOST|USER|PASS";
+            this.richTextBox2.DoubleClick += new System.EventHandler(this.richTextBox2_DoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 234);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 185);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 215);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Loads";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total Lines";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(13, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Invalid";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(13, 449);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Valid";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(169, 421);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(169, 464);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.Color.Green;
+            this.label6.Location = new System.Drawing.Point(169, 444);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(249, 526);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "cPanels Checker - By Mf4Tn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ cPanels Checker - By Mf4Tn/cPanels Checker - By Mf4Tn/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using Leaf.xNet;
+
+namespace cPanels_Checker___By_Mf4Tn
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog file = new OpenFileDialog();
+        int valid, invalid = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yes = 0;
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                List<string> check = new List<string>();
+                richTextBox1.Text = "";
+                foreach(var line in File.ReadAllLines(file.FileName))
+                {
+                    try
+                    {
+                        if(!check.Contains(line) && line.Split('|').Length == 3)
+                        {
+                            check.Add(line);
+                            yes++;
+                            richTextBox1.Text += line+"\n\n";
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                label4.Text = yes.ToString();
+                MessageBox.Show($"'{label4.Text}' cPanels Load !", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text != "")
+            {
+                List<string> check = new List<string>();
+                int yes = 0;
+                foreach (var line in richTextBox1.Text.Split('\n'))
+                {
+                    try
+                    {
+                        if (!check.Contains(line) && line.Split('|').Length == 3)
+                        {
+                            check.Add(line);
+                            yes++;
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                label4.Text = yes.ToString();
+                //MessageBox.Show($"'{label4.Text}' cPanels Load !", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+        private void richTextBox2_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox2.Text.Replace("\n\n", "\n"));
+            MessageBox.Show("Text Copied To Your Clipboard", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text != "" && richTextBox1.Text != "HOST|USER|PASS")
+            {
+                List<string> check = new List<string>();
+                richTextBox2.Text = "";
+                int finish = 0;
+                foreach(var cp in richTextBox1.Text.Split('\n'))
+                {
+                    
+                    if(cp != "" && !check.Contains(cp))
+                    {
+                        check.Add(cp);
+                        new Thread(() =>
+                        {
+                            try
+                            {
+                                HttpRequest request = new HttpRequest();
+                                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                                request.ConnectTimeout = 25000;
+                                var response = request.Post(cp.Split('|')[0] + "/login/?login_only=1", $"user={cp.Split('|')[1]}&pass={cp.Split('|')[2]}&goto_uri=%2F", "application/x-www-form-urlencoded");
+                                if (response.ToString().Contains("redirect\":\""))
+                                {
+                                    richTextBox2.Invoke(new MethodInvoker(delegate
+                                    {
+                                        richTextBox2.Text += cp + "\n\n";
+                                    }));
+                                    valid++;
+
+                                }
+                                else
+                                {
+                                    invalid++;
+                                }
+                            }
+                            catch (Exception ex){ invalid++; finish++; }
+                            label6.Invoke(new MethodInvoker(delegate
+                            {
+                                label6.Text = valid.ToString();
+                            }));
+                            label5.Invoke(new MethodInvoker(delegate
+                            {
+                                label5.Text = invalid.ToString();
+                            }));
+                        })
+                        { IsBackground = true }.Start();
+                    }
+
+                    
+
+                }
+                
+            }
+        }
+    }
+}
